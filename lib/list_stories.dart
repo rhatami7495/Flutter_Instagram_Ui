57218a45b760d6a985bffa410bc02b0ee8b739f1
r.hatami <r.hatami@rahkar.co>
2022-07-26 15:00:25 +0430

    added text to story item.

--- lib/list_stories.dart
+++ lib/list_stories.dart
@@ -30,34 +30,58 @@ class ListStories extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       itemCount: 8,
       itemBuilder: (context, index) {
-        return Stack(
-          alignment: Alignment.bottomRight,
+        return Column(
           children: [
-            Container(
-              width: 60.0,
-              height: 60.0,
-              decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                      fit: BoxFit.cover,
-                      image: NetworkImage(
-                          "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-19/229784066_1017825865697673_7736746121573483965_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=jffZ4mdAPowAX-AXJRC&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9bLGkA9JaEjfFrEvMZQ8Azw0J4UQLxZwaMuOl3JIpdVw&oe=62E630FA&_nc_sid=8fd12b"))),
-              margin: EdgeInsets.symmetric(horizontal: 5.0),
+            Stack(
+              alignment: Alignment.bottomRight,
+              children: [
+                Container(
+                  width: 60.0,
+                  height: 60.0,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              "https://scontent-iad3-1.cdninstagram.com/v/t51.2885-19/229784066_1017825865697673_7736746121573483965_n.jpg?stp=dst-jpg_s150x150&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=109&_nc_ohc=jffZ4mdAPowAX-AXJRC&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AT9bLGkA9JaEjfFrEvMZQ8Azw0J4UQLxZwaMuOl3JIpdVw&oe=62E630FA&_nc_sid=8fd12b"))),
+                  margin: EdgeInsets.symmetric(horizontal: 5.0),
+                ),
+                index == 0
+                    ? Positioned(
+                        right: 10.0,
+                        bottom: 0.0,
+                        child: CircleAvatar(
+                          backgroundColor: Colors.blueAccent,
+                          radius: 10.0,
+                          child: Icon(
+                            Icons.add,
+                            size: 14.0,
+                            color: Colors.white,
+                          ),
+                        ))
+                    : Container()
+              ],
             ),
             index == 0
-                ? Positioned(
-                    right: 10.0,
-                    bottom: 0.0,
-                    child: CircleAvatar(
-                      backgroundColor: Colors.blueAccent,
-                      radius: 10.0,
-                      child: Icon(
-                        Icons.add,
-                        size: 14.0,
-                        color: Colors.white,
-                      ),
-                    ))
-                : Container()
+                ? Text(
+                    "My Story",
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w400),
+
+                    textAlign: TextAlign.center,
+                  )
+                : Text(
+                    "Reza",
+                    style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w400),
+                    textAlign: TextAlign.center,
+                  )
           ],
         );
       },
